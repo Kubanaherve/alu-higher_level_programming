61e0ad5a9e64bcf447cd63c21833e18f31c3f641
Herve <h.kubana@alustudent.com>
2026-06-20 15:45:12 +0200

    Zen of Python is so cool

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-
 str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
-
-print(str[39:66] + " " + str[106:113])
+print(str[39:66] + str[66:67] + str[67:76])
