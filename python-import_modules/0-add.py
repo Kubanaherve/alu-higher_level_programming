6713ea647d993721f2c889c916b762a23727379d
Herve <h.kubana@alustudent.com>
2026-06-20 17:01:02 +0200

    Restructure 0-add.py: top-level a/b assignments, guard only the print call

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    add = __import__("add_0").add
+from add_0 import add
 
-    a = 1
-    b = 2
+a = 1
+b = 2
+
+if __name__ == "__main__":
     print("{} + {} = {}".format(a, b, add(a, b)))
