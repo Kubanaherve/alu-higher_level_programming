f529775c41196a42bf68fe00a6f8fcbb6b7a17de
Herve <h.kubana@alustudent.com>
2026-07-03 15:49:51 +0200

    2-safe_print_list_integers: implement skip-non-integers logic

--- 2-safe_print_list_integers.py
+++ 2-safe_print_list_integers.py
@@ -4,4 +4,12 @@
 
 def safe_print_list_integers(my_list=[], x=0):
     """Print only the integers among the first x elements of a list."""
-    pass
+    count = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            count += 1
+        except (ValueError, TypeError):
+            continue
+    print()
+    return count
