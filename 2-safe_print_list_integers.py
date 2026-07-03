17734c490d3f860119d899285de1cee305342fa3
Herve <h.kubana@alustudent.com>
2026-07-03 15:49:49 +0200

    2-safe_print_list_integers: add function signature and docstring

--- 2-safe_print_list_integers.py
+++ 2-safe_print_list_integers.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+"""Module for safe_print_list_integers."""
+
+
+def safe_print_list_integers(my_list=[], x=0):
+    """Print only the integers among the first x elements of a list."""
+    pass
