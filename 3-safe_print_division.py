8aad772b8946efa2644c309fbee28aff3ad659c4
Herve <h.kubana@alustudent.com>
2026-07-03 15:49:57 +0200

    3-safe_print_division: implement try/except/finally division

--- 3-safe_print_division.py
+++ 3-safe_print_division.py
@@ -4,4 +4,11 @@
 
 def safe_print_division(a, b):
     """Divide two integers and print the result, safely."""
-    pass
+    result = None
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        result = None
+    finally:
+        print("Inside result: {}".format(result))
+    return result
