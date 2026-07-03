d1d6026bd5a749036036ba26a852f40f012592b7
Herve <h.kubana@alustudent.com>
2026-07-03 15:49:39 +0200

    0-safe_print_list: implement try/except loop logic

--- 0-safe_print_list.py
+++ 0-safe_print_list.py
@@ -4,4 +4,12 @@
 
 def safe_print_list(my_list=[], x=0):
     """Print x elements of a list, safely."""
-    pass
+    count = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            count += 1
+        except IndexError:
+            break
+    print()
+    return count
