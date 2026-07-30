1f286fe4edc4091e19870cc8ca3a429b85f5c0e7
Herve <h.kubana@alustudent.com>
2026-07-30 14:15:48 +0200

    Remove sys import - only requests/urllib allowed per task rules

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
 """Fetches https://alu-intranet.hbtn.io/status using urllib."""
 import urllib.request
-import sys
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        url = sys.argv[1]
-    else:
-        url = "https://alu-intranet.hbtn.io/status"
+    url = "https://alu-intranet.hbtn.io/status"
     with urllib.request.urlopen(url) as response:
         body = response.read()
         print("Body response:")
--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
 """Fetches https://alu-intranet.hbtn.io/status"""
 import requests
-import sys
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        url = sys.argv[1]
-    else:
-        url = "https://alu-intranet.hbtn.io/status"
+    url = "https://alu-intranet.hbtn.io/status"
     response = requests.get(url)
     print("Body response:")
     print("\t- type: {}".format(type(response.text)))
