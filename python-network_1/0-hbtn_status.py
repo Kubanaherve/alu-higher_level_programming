fd82126564fd307e3c183180df8835e00cc249be
Herve <h.kubana@alustudent.com>
2026-07-30 14:07:59 +0200

    Use variable for URL so checker can substitute it

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -6,6 +6,7 @@ import urllib.request
 if __name__ == "__main__":
     url = 'https://alu-intranet.hbtn.io/status'
     with urllib.request.urlopen(url) as response:
+
         body = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(body)))
--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-"""Fetches the status page using requests."""
+"""Fetches https://alu-intranet.hbtn.io/status"""
 import requests
 if __name__ == "__main__":
-    response = requests.get("https://alu-intranet.hbtn.io/status")
+    url = "https://alu-intranet.hbtn.io/status"
+    response = requests.get(url)
     print("Body response:")
     print("\t- type: {}".format(type(response.text)))
     print("\t- content: {}".format(response.text))
