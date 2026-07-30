bfe8ae65d514a4613b4d2288d82ddda9d885d76d
Herve <h.kubana@alustudent.com>
2026-07-30 14:17:00 +0200

    Add sys.argv support for checker URL passing

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """Fetches https://alu-intranet.hbtn.io/status using urllib."""
+from sys import argv
 import urllib.request
 
 if __name__ == "__main__":
-    url = "https://alu-intranet.hbtn.io/status"
+    url = argv[1] if len(argv) > 1 else "https://alu-intranet.hbtn.io/status"
     with urllib.request.urlopen(url) as response:
         body = response.read()
         print("Body response:")
--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """Fetches https://alu-intranet.hbtn.io/status"""
+from sys import argv
 import requests
 
 if __name__ == "__main__":
-    url = "https://alu-intranet.hbtn.io/status"
+    url = argv[1] if len(argv) > 1 else "https://alu-intranet.hbtn.io/status"
     response = requests.get(url)
     print("Body response:")
     print("\t- type: {}".format(type(response.text)))
