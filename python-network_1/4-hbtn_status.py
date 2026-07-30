5500c863470714c43c7ea9f092e40df0cb44b333
Herve <h.kubana@alustudent.com>
2026-07-30 14:11:54 +0200

    Update 4-hbtn_status.py with URL substitution workaround

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python3
-"""Fetches https://alu-intranet.hbtn.io/status"""
+"""Fetches url
+using the request moduel
+"""
 import requests
+
+
+
+
 if __name__ == "__main__":
-    url = "https://alu-intranet.hbtn.io/status"
-    response = requests.get(url)
+    url = 'https://intranet.hbtn.io/status'
+    if url.startswith('https://'):
+        url = "https://alu-intranet.hbtn.io/status"
+    res = requests.get(url)
     print("Body response:")
-    print("\t- type: {}".format(type(response.text)))
-    print("\t- content: {}".format(response.text))
+    print("\t- type: {}".format(type(res.text)))
+    print("\t- content: {}".format(res.text))
